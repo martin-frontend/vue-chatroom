6db4ed9f3687c58fe4f29905ca8c5fc9c92090be
martin <martinfrontend@gmail.com>
2021-02-02 09:27:09 +0800

    socket

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <button @click="clickButton">11111111</button>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span>{{ $socket.connected ? 'Connected' : 'Disconnected' }}</span>
   </div>
 </template>
 
@@ -12,6 +14,28 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  sockets: {
+    connect() {
+      console.log('socket connected')
+    },
+    customEmit(val) {
+      console.log(val,'this method was fired by the socket server. eg: io.emit("customEmit", data)')
+    },
+    res (data) {
+      this.msgList.push(data)
+      console.log('接收到服务端消息', data);
+    }
+  },
+  methods: {
+    clickButton() {
+      // this.$socket.client is `socket.io-client` instance
+      this.$socket.emit('msgToServer', '123');
+    }
+  },
+  mounted () {
+    // console.log(this.$socket);
+    // this.clickButton()
   }
 }
 </script>
